e2fa9576ed095aa78c7285d2d7d0f6ddc999e088
Maidi <mareike.haug@web.de>
2022-07-19 16:21:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,553 @@
+/* eslint-disable */
+
+window.helme = [
+  {
+    Suit: "Mark1",
+    Strength: 2,
+    Agility: 1,
+    Speed: 1,
+    Price: 1,
+    Protection: 3,
+    Weight: 8,
+    Image: "Mark_I_Kopf.png"
+  },
+  {
+    Suit: "Mark2",
+    Strength: 2,
+    Agility: 5,
+    Speed: 5,
+    Price: 1,
+    Protection: 5,
+    Weight: 4,
+    Image: "Mark_II_Kopf.png"
+  },
+  {
+    Suit: "Warmachine",
+    Strength: 2,
+    Agility: 5,
+    Speed: 5,
+    Price: 1,
+    Protection: 6,
+    Weight: 5,
+    Image: "Warmachine_Kopf.png"
+  },
+  {
+    Suit: "Mark3",
+    Strength: 2,
+    Agility: 5,
+    Speed: 5,
+    Price: 1,
+    Protection: 6,
+    Weight: 4,
+    Image: "Mark_III_Kopf.png"
+  },
+  {
+    Suit: "Mark6",
+    Strength: 2,
+    Agility: 6,
+    Speed: 6,
+    Price: 1,
+    Protection: 6,
+    Weight: 4,
+    Image: "Mark_VI_Kopf.png"
+  },
+  {
+    Suit: "MarkHulk",
+    Strength: 4,
+    Agility: 2,
+    Speed: 2,
+    Price: 1,
+    Protection: 9,
+    Weight: 9,
+    Image: "Mark_XLVIII_Kopf.png"
+  },
+  {
+    Suit: "Mark41",
+    Strength: 1,
+    Agility: 7,
+    Speed: 7,
+    Price: 1,
+    Protection: 4,
+    Weight: 2,
+    Image: "Mark_XLI_Kopf.png"
+  },
+  {
+    Suit: "Mark42",
+    Strength: 2,
+    Agility: 5,
+    Speed: 5,
+    Price: 1,
+    Protection: 5,
+    Weight: 4,
+    Image: "Mark_XLVII_Kopf.png"
+  },
+  {
+    Suit: "Mark50",
+    Strength: 2,
+    Agility: 10,
+    Speed: 10,
+    Price: 1,
+    Protection: 8,
+    Weight: 1,
+    Image: "Mark_L_Kopf.png"
+  }
+];
+window.körper = [
+  {
+    Suit: "Mark1",
+    Strength: 4,
+    Agility: 1,
+    Speed: 1,
+    Price: 1,
+    Protection: 7,
+    Weight: 10,
+    Image: "Mark_I_Körper.png"
+  },
+  {
+    Suit: "Mark2",
+    Strength: 3,
+    Agility: 4,
+    Speed: 4,
+    Price: 1,
+    Protection: 4,
+    Weight: 5,
+    Image: "Mark_II_Körper.png"
+  },
+  {
+    Suit: "Warmachine",
+    Strength: 6,
+    Agility: 3,
+    Speed: 3,
+    Price: 1,
+    Protection: 5,
+    Weight: 6,
+    Image: "Warmachine_Körper.png"
+  },
+  {
+    Suit: "Mark3",
+    Strength: 5,
+    Agility: 5,
+    Speed: 5,
+    Price: 1,
+    Protection: 4,
+    Weight: 5,
+    Image: "Mark_III_Körper.png"
+  },
+  {
+    Suit: "Mark6",
+    Strength: 6,
+    Agility: 5,
+    Speed: 5,
+    Price: 1,
+    Protection: 5,
+    Weight: 5,
+    Image: "Mark_VI_Körper.png"
+  },
+  {
+    Suit: "MarkHulk",
+    Strength: 9,
+    Agility: 1,
+    Speed: 1,
+    Price: 1,
+    Protection: 10,
+    Weight: 10,
+    Image: "Mark_XLVIII_Körper.png"
+  },
+  {
+    Suit: "Mark41",
+    Strength: 3,
+    Agility: 8,
+    Speed: 8,
+    Price: 1,
+    Protection: 4,
+    Weight: 3,
+    Image: "Mark_XLI_Körper.png"
+  },
+  {
+    Suit: "Mark42",
+    Strength: 5,
+    Agility: 7,
+    Speed: 6,
+    Price: 1,
+    Protection: 6,
+    Weight: 4,
+    Image: "Mark_XLVII_Körper.png"
+  },
+  {
+    Suit: "Mark50",
+    Strength: 10,
+    Agility: 10,
+    Speed: 10,
+    Price: 1,
+    Protection: 5,
+    Weight: 1,
+    Image: "Mark_L_Körper.png"
+  }
+];
+window.arme = [
+  {
+    Suit: "Mark1",
+    Strength: 4,
+    Agility: 1,
+    Speed: 1,
+    Price: 1,
+    Protection: 3,
+    Weight: 7,
+    Image: "Mark_I_Arme.png"
+  },
+  {
+    Suit: "Mark2",
+    Strength: 5,
+    Agility: 4,
+    Speed: 4,
+    Price: 1,
+    Protection: 5,
+    Weight: 3,
+    Image: "Mark_II_Arme.png"
+  },
+  {
+    Suit: "Warmachine",
+    Strength: 6,
+    Agility: 3,
+    Speed: 3,
+    Price: 1,
+    Protection: 6,
+    Weight: 4,
+    Image: "Warmachine_Arme.png"
+  },
+  {
+    Suit: "Mark3",
+    Strength: 6,
+    Agility: 4,
+    Speed: 4,
+    Price: 1,
+    Protection: 6,
+    Weight: 4,
+    Image: "Mark_III_Arme.png"
+  },
+  {
+    Suit: "Mark6",
+    Strength: 6,
+    Agility: 5,
+    Speed: 5,
+    Price: 1,
+    Protection: 6,
+    Weight: 3,
+    Image: "Mark_VI_Arme.png"
+  },
+  {
+    Suit: "MarkHulk",
+    Strength: 8,
+    Agility: 2,
+    Speed: 3,
+    Price: 1,
+    Protection: 8,
+    Weight: 8,
+    Image: "Mark_XLVIII_Arme.png"
+  },
+  {
+    Suit: "Mark41",
+    Strength: 4,
+    Agility: 10,
+    Speed: 10,
+    Price: 1,
+    Protection: 4,
+    Weight: 2,
+    Image: "Mark_XLI_Arme.png"
+  },
+  {
+    Suit: "Mark42",
+    Strength: 5,
+    Agility: 8,
+    Speed: 8,
+    Price: 1,
+    Protection: 5,
+    Weight: 3,
+    Image: "Mark_XLII_Arme.png"
+  },
+  {
+    Suit: "Mark50",
+    Strength: 10,
+    Agility: 10,
+    Speed: 10,
+    Price: 1,
+    Protection: 5,
+    Weight: 1,
+    Image: "Mark_L_Arme.png"
+  }
+];
+window.beine = [
+  {
+    Suit: "Mark1",
+    Strength: 1,
+    Agility: 1,
+    Speed: 1,
+    Price: 1,
+    Protection: 2,
+    Weight: 7,
+    Image: "Mark_I_Beine.png"
+  },
+  {
+    Suit: "Mark2",
+    Strength: 3,
+    Agility: 4,
+    Speed: 4,
+    Price: 1,
+    Protection: 4,
+    Weight: 4,
+    Image: "Mark_II_Beine.png"
+  },
+  {
+    Suit: "Warmachine",
+    Strength: 4,
+    Agility: 3,
+    Speed: 3,
+    Price: 1,
+    Protection: 5,
+    Weight: 5,
+    Image: "Mark_XLVIII_Beine.png"
+  },
+  {
+    Suit: "Mark3",
+    Strength: 4,
+    Agility: 4,
+    Speed: 4,
+    Price: 1,
+    Protection: 5,
+    Weight: 4,
+    Image: "Mark_III_Beine.png"
+  },
+  {
+    Suit: "Mark6",
+    Strength: 5,
+    Agility: 5,
+    Speed: 5,
+    Price: 1,
+    Protection: 5,
+    Weight: 4,
+    Image: "Mark_VI_Beine.png"
+  },
+  {
+    Suit: "MarkHulk",
+    Strength: 7,
+    Agility: 1,
+    Speed: 3,
+    Price: 1,
+    Protection: 8,
+    Weight: 9,
+    Image: "Mark_XLVIII_Beine.png"
+  },
+  {
+    Suit: "Mark41",
+    Strength: 3,
+    Agility: 9,
+    Speed: 9,
+    Price: 1,
+    Protection: 4,
+    Weight: 3,
+    Image: "Mark_XLI_Beine.png"
+  },
+  {
+    Suit: "Mark42",
+    Strength: 4,
+    Agility: 7,
+    Speed: 7,
+    Price: 1,
+    Protection: 6,
+    Weight: 4,
+    Image: "Mark_XLII_Beine.png"
+  },
+  {
+    Suit: "Mark50",
+    Strength: 10,
+    Agility: 10,
+    Speed: 10,
+    Price: 1,
+    Protection: 5,
+    Weight: 1,
+    Image: "Mark_L_Beine.png"
+  }
+];
+
+var selectedHelmet = helme[0];
+var selectedBody = körper[0];
+var selectedLegs = beine[0];
+var selectedArms = arme[0];
+var selectedSuit;
+
+const calculateStat = ({
+  selectedHelmet,
+  selectedBody,
+  selectedLegs,
+  selectedArms,
+  helmetWeight = 0.5,
+  bodyWeight = 2,
+  key
+}) => {
+  return (
+    (selectedHelmet[key] * helmetWeight +
+      selectedBody[key] * bodyWeight +
+      selectedLegs[key] +
+      selectedArms[key]) /
+    4
+  );
+};
+
+const updateSelectedSuit = () => {
+  selectedSuit = {
+    Strength: calculateStat({
+      selectedArms,
+      selectedBody,
+      selectedHelmet,
+      selectedLegs,
+      key: "Strength"
+    }),
+    Agility: calculateStat({
+      selectedArms,
+      selectedBody,
+      selectedHelmet,
+      selectedLegs,
+      key: "Agility"
+    }),
+    Speed: calculateStat({
+      selectedArms,
+      selectedBody,
+      selectedHelmet,
+      selectedLegs,
+      key: "Speed"
+    }),
+    Price: calculateStat({
+      selectedArms,
+      selectedBody,
+      selectedHelmet,
+      selectedLegs,
+      key: "Price"
+    }),
+    Protection: calculateStat({
+      selectedArms,
+      selectedBody,
+      selectedHelmet,
+      selectedLegs,
+      key: "Protection"
+    }),
+    Weight: calculateStat({
+      selectedArms,
+      selectedBody,
+      selectedHelmet,
+      selectedLegs,
+      key: "Weight"
+    })
+  };
+};
+
+window.mute = function () {
+  var BgMusic = document.getElementById("Backgroundmusic");
+  var unmutedemoji = "🔊";
+  var mutedemoji = "🔇";
+  var MuteButton = document.getElementById("Pika");
+  if (BgMusic.muted) {
+    MuteButton.innerText = unmutedemoji;
+    BgMusic.play();
+    BgMusic.muted = false;
+  } else {
+    MuteButton.innerText = mutedemoji;
+    BgMusic.muted = true;
+    BgMusic.pause();
+  }
+};
+
+window.changeGitter = (element, selectedElementKey) => {
+  const gitter = document.querySelector(".gitter");
+  gitter.innerHTML = element
+    .map(
+      (elm, index) =>
+        `<button class="select" id="${index}">
+        <img class="grid-image" src="/assets/${elm.Image}"/>
+        </button>`
+    )
+    .join("");
+  document.querySelectorAll(".select").forEach(
+    (select) =>
+      (select.onclick = (button) => {
+        const targetId = element[button.target.id];
+        window[selectedElementKey] = targetId;
+        updateSelectedSuit();
+        updateChartData([
+          selectedSuit.Strength,
+          selectedSuit.Speed,
+          selectedSuit.Agility,
+          selectedSuit.Weight,
+          selectedSuit.Protection
+        ]);
+      })
+  );
+};
+
+const ctx = document.getElementById("myChart");
+const myChart = new Chart(ctx, {
+  type: "radar",
+  data: {
+    labels: ["Strength", "Speed", "Agility", "Weight", "Protection"],
+    datasets: [
+      {
+        label: "Statistics",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.9)",
+          "rgba(54, 162, 235, 0.9)",
+          "rgba(255, 206, 86, 0.9)",
+          "rgba(75, 192, 192, 0.9)",
+          "rgba(75, 192, 192, 0.9)"
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(75, 192, 192, 1)"
+        ],
+        color: "#ffffff",
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    responsive: false,
+    maintainAspectRatio: true,
+    scale: {
+      min: 0,
+      max: 10
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+    scales: {
+      r: {
+        pointLabels: {
+          color: "white",
+          font: {
+            size: 10
+          }
+        },
+        grid: {
+          color: "transparent"
+        },
+        angleLines: {
+          color: "white"
+        },
+        ticks: {
+          callback: function () {
+            return "";
+          },
+          backdropColor: "rgba(0, 0, 0, 0)"
+        }
+      }
+    }
+  }
+});
+
+window.updateChartData = (data) => {
+  myChart.data.datasets[0].data = data;
+  myChart.update();
+};
