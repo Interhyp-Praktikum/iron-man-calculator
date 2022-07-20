6861841fbbdcc12e37c6bc817bb830c27d8c380b
Maidi <mareike.haug@web.de>
2022-07-20 08:57:59 +0200

    tidy up

--- script.js
+++ script.js
@@ -1,384 +1,12 @@
 /* eslint-disable */
 
-window.helme = [
-  {
-    Suit: "Mark1",
-    Strength: 2,
-    Agility: 1,
-    Speed: 1,
-    Price: 1,
-    Protection: 3,
-    Weight: 8,
-    Image: "Mark_I_Kopf.png"
-  },
-  {
-    Suit: "Mark2",
-    Strength: 2,
-    Agility: 5,
-    Speed: 5,
-    Price: 1,
-    Protection: 5,
-    Weight: 4,
-    Image: "Mark_II_Kopf.png"
-  },
-  {
-    Suit: "Warmachine",
-    Strength: 2,
-    Agility: 5,
-    Speed: 5,
-    Price: 1,
-    Protection: 6,
-    Weight: 5,
-    Image: "Warmachine_Kopf.png"
-  },
-  {
-    Suit: "Mark3",
-    Strength: 2,
-    Agility: 5,
-    Speed: 5,
-    Price: 1,
-    Protection: 6,
-    Weight: 4,
-    Image: "Mark_III_Kopf.png"
-  },
-  {
-    Suit: "Mark6",
-    Strength: 2,
-    Agility: 6,
-    Speed: 6,
-    Price: 1,
-    Protection: 6,
-    Weight: 4,
-    Image: "Mark_VI_Kopf.png"
-  },
-  {
-    Suit: "MarkHulk",
-    Strength: 4,
-    Agility: 2,
-    Speed: 2,
-    Price: 1,
-    Protection: 9,
-    Weight: 9,
-    Image: "Mark_XLVIII_Kopf.png"
-  },
-  {
-    Suit: "Mark41",
-    Strength: 1,
-    Agility: 7,
-    Speed: 7,
-    Price: 1,
-    Protection: 4,
-    Weight: 2,
-    Image: "Mark_XLI_Kopf.png"
-  },
-  {
-    Suit: "Mark42",
-    Strength: 2,
-    Agility: 5,
-    Speed: 5,
-    Price: 1,
-    Protection: 5,
-    Weight: 4,
-    Image: "Mark_XLVII_Kopf.png"
-  },
-  {
-    Suit: "Mark50",
-    Strength: 2,
-    Agility: 10,
-    Speed: 10,
-    Price: 1,
-    Protection: 8,
-    Weight: 1,
-    Image: "Mark_L_Kopf.png"
-  }
-];
-window.körper = [
-  {
-    Suit: "Mark1",
-    Strength: 4,
-    Agility: 1,
-    Speed: 1,
-    Price: 1,
-    Protection: 7,
-    Weight: 10,
-    Image: "Mark_I_Körper.png"
-  },
-  {
-    Suit: "Mark2",
-    Strength: 3,
-    Agility: 4,
-    Speed: 4,
-    Price: 1,
-    Protection: 4,
-    Weight: 5,
-    Image: "Mark_II_Körper.png"
-  },
-  {
-    Suit: "Warmachine",
-    Strength: 6,
-    Agility: 3,
-    Speed: 3,
-    Price: 1,
-    Protection: 5,
-    Weight: 6,
-    Image: "Warmachine_Körper.png"
-  },
-  {
-    Suit: "Mark3",
-    Strength: 5,
-    Agility: 5,
-    Speed: 5,
-    Price: 1,
-    Protection: 4,
-    Weight: 5,
-    Image: "Mark_III_Körper.png"
-  },
-  {
-    Suit: "Mark6",
-    Strength: 6,
-    Agility: 5,
-    Speed: 5,
-    Price: 1,
-    Protection: 5,
-    Weight: 5,
-    Image: "Mark_VI_Körper.png"
-  },
-  {
-    Suit: "MarkHulk",
-    Strength: 9,
-    Agility: 1,
-    Speed: 1,
-    Price: 1,
-    Protection: 10,
-    Weight: 10,
-    Image: "Mark_XLVIII_Körper.png"
-  },
-  {
-    Suit: "Mark41",
-    Strength: 3,
-    Agility: 8,
-    Speed: 8,
-    Price: 1,
-    Protection: 4,
-    Weight: 3,
-    Image: "Mark_XLI_Körper.png"
-  },
-  {
-    Suit: "Mark42",
-    Strength: 5,
-    Agility: 7,
-    Speed: 6,
-    Price: 1,
-    Protection: 6,
-    Weight: 4,
-    Image: "Mark_XLVII_Körper.png"
-  },
-  {
-    Suit: "Mark50",
-    Strength: 10,
-    Agility: 10,
-    Speed: 10,
-    Price: 1,
-    Protection: 5,
-    Weight: 1,
-    Image: "Mark_L_Körper.png"
-  }
-];
-window.arme = [
-  {
-    Suit: "Mark1",
-    Strength: 4,
-    Agility: 1,
-    Speed: 1,
-    Price: 1,
-    Protection: 3,
-    Weight: 7,
-    Image: "Mark_I_Arme.png"
-  },
-  {
-    Suit: "Mark2",
-    Strength: 5,
-    Agility: 4,
-    Speed: 4,
-    Price: 1,
-    Protection: 5,
-    Weight: 3,
-    Image: "Mark_II_Arme.png"
-  },
-  {
-    Suit: "Warmachine",
-    Strength: 6,
-    Agility: 3,
-    Speed: 3,
-    Price: 1,
-    Protection: 6,
-    Weight: 4,
-    Image: "Warmachine_Arme.png"
-  },
-  {
-    Suit: "Mark3",
-    Strength: 6,
-    Agility: 4,
-    Speed: 4,
-    Price: 1,
-    Protection: 6,
-    Weight: 4,
-    Image: "Mark_III_Arme.png"
-  },
-  {
-    Suit: "Mark6",
-    Strength: 6,
-    Agility: 5,
-    Speed: 5,
-    Price: 1,
-    Protection: 6,
-    Weight: 3,
-    Image: "Mark_VI_Arme.png"
-  },
-  {
-    Suit: "MarkHulk",
-    Strength: 8,
-    Agility: 2,
-    Speed: 3,
-    Price: 1,
-    Protection: 8,
-    Weight: 8,
-    Image: "Mark_XLVIII_Arme.png"
-  },
-  {
-    Suit: "Mark41",
-    Strength: 4,
-    Agility: 10,
-    Speed: 10,
-    Price: 1,
-    Protection: 4,
-    Weight: 2,
-    Image: "Mark_XLI_Arme.png"
-  },
-  {
-    Suit: "Mark42",
-    Strength: 5,
-    Agility: 8,
-    Speed: 8,
-    Price: 1,
-    Protection: 5,
-    Weight: 3,
-    Image: "Mark_XLII_Arme.png"
-  },
-  {
-    Suit: "Mark50",
-    Strength: 10,
-    Agility: 10,
-    Speed: 10,
-    Price: 1,
-    Protection: 5,
-    Weight: 1,
-    Image: "Mark_L_Arme.png"
-  }
-];
-window.beine = [
-  {
-    Suit: "Mark1",
-    Strength: 1,
-    Agility: 1,
-    Speed: 1,
-    Price: 1,
-    Protection: 2,
-    Weight: 7,
-    Image: "Mark_I_Beine.png"
-  },
-  {
-    Suit: "Mark2",
-    Strength: 3,
-    Agility: 4,
-    Speed: 4,
-    Price: 1,
-    Protection: 4,
-    Weight: 4,
-    Image: "Mark_II_Beine.png"
-  },
-  {
-    Suit: "Warmachine",
-    Strength: 4,
-    Agility: 3,
-    Speed: 3,
-    Price: 1,
-    Protection: 5,
-    Weight: 5,
-    Image: "Mark_XLVIII_Beine.png"
-  },
-  {
-    Suit: "Mark3",
-    Strength: 4,
-    Agility: 4,
-    Speed: 4,
-    Price: 1,
-    Protection: 5,
-    Weight: 4,
-    Image: "Mark_III_Beine.png"
-  },
-  {
-    Suit: "Mark6",
-    Strength: 5,
-    Agility: 5,
-    Speed: 5,
-    Price: 1,
-    Protection: 5,
-    Weight: 4,
-    Image: "Mark_VI_Beine.png"
-  },
-  {
-    Suit: "MarkHulk",
-    Strength: 7,
-    Agility: 1,
-    Speed: 3,
-    Price: 1,
-    Protection: 8,
-    Weight: 9,
-    Image: "Mark_XLVIII_Beine.png"
-  },
-  {
-    Suit: "Mark41",
-    Strength: 3,
-    Agility: 9,
-    Speed: 9,
-    Price: 1,
-    Protection: 4,
-    Weight: 3,
-    Image: "Mark_XLI_Beine.png"
-  },
-  {
-    Suit: "Mark42",
-    Strength: 4,
-    Agility: 7,
-    Speed: 7,
-    Price: 1,
-    Protection: 6,
-    Weight: 4,
-    Image: "Mark_XLII_Beine.png"
-  },
-  {
-    Suit: "Mark50",
-    Strength: 10,
-    Agility: 10,
-    Speed: 10,
-    Price: 1,
-    Protection: 5,
-    Weight: 1,
-    Image: "Mark_L_Beine.png"
-  }
-];
+let summedUpStats;
 
-var selectedHelmet = helme[0];
-var selectedBody = körper[0];
-var selectedLegs = beine[0];
-var selectedArms = arme[0];
-var selectedSuit;
 const selectedItems = {
-  selectedHelmet: helme[0],
-  selectedArms: arme[0],
-  selectedBody: körper[0],
-  selectedLegs: beine[0]
+  selectedHelmet: window.helme[0],
+  selectedArms: window.arme[0],
+  selectedBody: window.körper[0],
+  selectedLegs: window.beine[0]
 };
 
 const calculateStat = ({
@@ -399,8 +27,8 @@ const calculateStat = ({
   );
 };
 
-const updateSelectedSuit = () => {
-  selectedSuit = {
+const updateSummedUpStats = () => {
+  summedUpStats = {
     Strength: calculateStat({
       selectedArms: selectedItems.selectedArms,
       selectedBody: selectedItems.selectedBody,
@@ -462,29 +90,28 @@ window.mute = function () {
   }
 };
 
-window.changeGitter = (element, selectedElementKey) => {
-  const gitter = document.querySelector(".gitter");
-  gitter.innerHTML = element
+window.changeItemsInGrid = (allElements, selectedElementKey) => {
+  const gitter = document.querySelector(".grid");
+  gitter.innerHTML = allElements
     .map(
-      (elm, index) =>
-        `<button class="select" id="${index}">
-        <img class="grid-image" src="/assets/${elm.Image}"/>
+      (element, index) =>
+        `<button class="grid-button" id="${index}">
+            <img class="grid-image" src="/assets/${element.Image}"/>
         </button>`
     )
     .join("");
-  document.querySelectorAll(".select").forEach(
+  document.querySelectorAll(".grid-button").forEach(
     (select) =>
       (select.onclick = (button) => {
-        const selectedElement = element[button.target.id];
+        const selectedElement = allElements[button.target.id];
         selectedItems[selectedElementKey] = selectedElement;
-        console.log(selectedHelmet, selectedElement);
-        updateSelectedSuit();
+        updateSummedUpStats();
         updateChartData([
-          selectedSuit.Strength,
-          selectedSuit.Speed,
-          selectedSuit.Agility,
-          selectedSuit.Weight,
-          selectedSuit.Protection
+          summedUpStats.Strength,
+          summedUpStats.Speed,
+          summedUpStats.Agility,
+          summedUpStats.Weight,
+          summedUpStats.Protection
         ]);
       })
   );
---
+++ stats.js
@@ -0,0 +1,369 @@
+window.helme = [
+    {
+      Suit: "Mark1",
+      Strength: 2,
+      Agility: 1,
+      Speed: 1,
+      Price: 1,
+      Protection: 3,
+      Weight: 8,
+      Image: "Mark_I_Kopf.png"
+    },
+    {
+      Suit: "Mark2",
+      Strength: 2,
+      Agility: 5,
+      Speed: 5,
+      Price: 1,
+      Protection: 5,
+      Weight: 4,
+      Image: "Mark_II_Kopf.png"
+    },
+    {
+      Suit: "Warmachine",
+      Strength: 2,
+      Agility: 5,
+      Speed: 5,
+      Price: 1,
+      Protection: 6,
+      Weight: 5,
+      Image: "Warmachine_Kopf.png"
+    },
+    {
+      Suit: "Mark3",
+      Strength: 2,
+      Agility: 5,
+      Speed: 5,
+      Price: 1,
+      Protection: 6,
+      Weight: 4,
+      Image: "Mark_III_Kopf.png"
+    },
+    {
+      Suit: "Mark6",
+      Strength: 2,
+      Agility: 6,
+      Speed: 6,
+      Price: 1,
+      Protection: 6,
+      Weight: 4,
+      Image: "Mark_VI_Kopf.png"
+    },
+    {
+      Suit: "MarkHulk",
+      Strength: 4,
+      Agility: 2,
+      Speed: 2,
+      Price: 1,
+      Protection: 9,
+      Weight: 9,
+      Image: "Mark_XLVIII_Kopf.png"
+    },
+    {
+      Suit: "Mark41",
+      Strength: 1,
+      Agility: 7,
+      Speed: 7,
+      Price: 1,
+      Protection: 4,
+      Weight: 2,
+      Image: "Mark_XLI_Kopf.png"
+    },
+    {
+      Suit: "Mark42",
+      Strength: 2,
+      Agility: 5,
+      Speed: 5,
+      Price: 1,
+      Protection: 5,
+      Weight: 4,
+      Image: "Mark_XLVII_Kopf.png"
+    },
+    {
+      Suit: "Mark50",
+      Strength: 2,
+      Agility: 10,
+      Speed: 10,
+      Price: 1,
+      Protection: 8,
+      Weight: 1,
+      Image: "Mark_L_Kopf.png"
+    }
+  ];
+  window.körper = [
+    {
+      Suit: "Mark1",
+      Strength: 4,
+      Agility: 1,
+      Speed: 1,
+      Price: 1,
+      Protection: 7,
+      Weight: 10,
+      Image: "Mark_I_Körper.png"
+    },
+    {
+      Suit: "Mark2",
+      Strength: 3,
+      Agility: 4,
+      Speed: 4,
+      Price: 1,
+      Protection: 4,
+      Weight: 5,
+      Image: "Mark_II_Körper.png"
+    },
+    {
+      Suit: "Warmachine",
+      Strength: 6,
+      Agility: 3,
+      Speed: 3,
+      Price: 1,
+      Protection: 5,
+      Weight: 6,
+      Image: "Warmachine_Körper.png"
+    },
+    {
+      Suit: "Mark3",
+      Strength: 5,
+      Agility: 5,
+      Speed: 5,
+      Price: 1,
+      Protection: 4,
+      Weight: 5,
+      Image: "Mark_III_Körper.png"
+    },
+    {
+      Suit: "Mark6",
+      Strength: 6,
+      Agility: 5,
+      Speed: 5,
+      Price: 1,
+      Protection: 5,
+      Weight: 5,
+      Image: "Mark_VI_Körper.png"
+    },
+    {
+      Suit: "MarkHulk",
+      Strength: 9,
+      Agility: 1,
+      Speed: 1,
+      Price: 1,
+      Protection: 10,
+      Weight: 10,
+      Image: "Mark_XLVIII_Körper.png"
+    },
+    {
+      Suit: "Mark41",
+      Strength: 3,
+      Agility: 8,
+      Speed: 8,
+      Price: 1,
+      Protection: 4,
+      Weight: 3,
+      Image: "Mark_XLI_Körper.png"
+    },
+    {
+      Suit: "Mark42",
+      Strength: 5,
+      Agility: 7,
+      Speed: 6,
+      Price: 1,
+      Protection: 6,
+      Weight: 4,
+      Image: "Mark_XLVII_Körper.png"
+    },
+    {
+      Suit: "Mark50",
+      Strength: 10,
+      Agility: 10,
+      Speed: 10,
+      Price: 1,
+      Protection: 5,
+      Weight: 1,
+      Image: "Mark_L_Körper.png"
+    }
+  ];
+  window.arme = [
+    {
+      Suit: "Mark1",
+      Strength: 4,
+      Agility: 1,
+      Speed: 1,
+      Price: 1,
+      Protection: 3,
+      Weight: 7,
+      Image: "Mark_I_Arme.png"
+    },
+    {
+      Suit: "Mark2",
+      Strength: 5,
+      Agility: 4,
+      Speed: 4,
+      Price: 1,
+      Protection: 5,
+      Weight: 3,
+      Image: "Mark_II_Arme.png"
+    },
+    {
+      Suit: "Warmachine",
+      Strength: 6,
+      Agility: 3,
+      Speed: 3,
+      Price: 1,
+      Protection: 6,
+      Weight: 4,
+      Image: "Warmachine_Arme.png"
+    },
+    {
+      Suit: "Mark3",
+      Strength: 6,
+      Agility: 4,
+      Speed: 4,
+      Price: 1,
+      Protection: 6,
+      Weight: 4,
+      Image: "Mark_III_Arme.png"
+    },
+    {
+      Suit: "Mark6",
+      Strength: 6,
+      Agility: 5,
+      Speed: 5,
+      Price: 1,
+      Protection: 6,
+      Weight: 3,
+      Image: "Mark_VI_Arme.png"
+    },
+    {
+      Suit: "MarkHulk",
+      Strength: 8,
+      Agility: 2,
+      Speed: 3,
+      Price: 1,
+      Protection: 8,
+      Weight: 8,
+      Image: "Mark_XLVIII_Arme.png"
+    },
+    {
+      Suit: "Mark41",
+      Strength: 4,
+      Agility: 10,
+      Speed: 10,
+      Price: 1,
+      Protection: 4,
+      Weight: 2,
+      Image: "Mark_XLI_Arme.png"
+    },
+    {
+      Suit: "Mark42",
+      Strength: 5,
+      Agility: 8,
+      Speed: 8,
+      Price: 1,
+      Protection: 5,
+      Weight: 3,
+      Image: "Mark_XLII_Arme.png"
+    },
+    {
+      Suit: "Mark50",
+      Strength: 10,
+      Agility: 10,
+      Speed: 10,
+      Price: 1,
+      Protection: 5,
+      Weight: 1,
+      Image: "Mark_L_Arme.png"
+    }
+  ];
+  window.beine = [
+    {
+      Suit: "Mark1",
+      Strength: 1,
+      Agility: 1,
+      Speed: 1,
+      Price: 1,
+      Protection: 2,
+      Weight: 7,
+      Image: "Mark_I_Beine.png"
+    },
+    {
+      Suit: "Mark2",
+      Strength: 3,
+      Agility: 4,
+      Speed: 4,
+      Price: 1,
+      Protection: 4,
+      Weight: 4,
+      Image: "Mark_II_Beine.png"
+    },
+    {
+      Suit: "Warmachine",
+      Strength: 4,
+      Agility: 3,
+      Speed: 3,
+      Price: 1,
+      Protection: 5,
+      Weight: 5,
+      Image: "Mark_XLVIII_Beine.png"
+    },
+    {
+      Suit: "Mark3",
+      Strength: 4,
+      Agility: 4,
+      Speed: 4,
+      Price: 1,
+      Protection: 5,
+      Weight: 4,
+      Image: "Mark_III_Beine.png"
+    },
+    {
+      Suit: "Mark6",
+      Strength: 5,
+      Agility: 5,
+      Speed: 5,
+      Price: 1,
+      Protection: 5,
+      Weight: 4,
+      Image: "Mark_VI_Beine.png"
+    },
+    {
+      Suit: "MarkHulk",
+      Strength: 7,
+      Agility: 1,
+      Speed: 3,
+      Price: 1,
+      Protection: 8,
+      Weight: 9,
+      Image: "Mark_XLVIII_Beine.png"
+    },
+    {
+      Suit: "Mark41",
+      Strength: 3,
+      Agility: 9,
+      Speed: 9,
+      Price: 1,
+      Protection: 4,
+      Weight: 3,
+      Image: "Mark_XLI_Beine.png"
+    },
+    {
+      Suit: "Mark42",
+      Strength: 4,
+      Agility: 7,
+      Speed: 7,
+      Price: 1,
+      Protection: 6,
+      Weight: 4,
+      Image: "Mark_XLII_Beine.png"
+    },
+    {
+      Suit: "Mark50",
+      Strength: 10,
+      Agility: 10,
+      Speed: 10,
+      Price: 1,
+      Protection: 5,
+      Weight: 1,
+      Image: "Mark_L_Beine.png"
+    }
+  ];
+  
